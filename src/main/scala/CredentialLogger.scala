ebfca21aef5734e312e62efca3e0daa6cb11bab3
HamingnottT <jnottingham836@yahoo.com>
2022-01-18 16:42:44 -0600

    Scala: Started on Sign-in Prompt

--- src/main/scala/CredentialLogger.scala
+++ src/main/scala/CredentialLogger.scala
@@ -1,3 +1,55 @@
-object CredentialLogger {
+//class CredentialLogger(){
+  import scala.io.StdIn._
 
-}
+
+
+
+  object CredentialLogger {
+    private var sysUserName = ""
+    private var sysPassword = ""
+
+
+    //def setUp(): Unit = {}
+
+    def prompt(): Unit = {
+
+
+      println(
+        s"""
+           |Hello, This is my first programming project that will log your website sign-in data for easy recall.
+           |WARNING: Authentication required before proceeding.
+           |""".stripMargin)
+
+      var doOnce = false
+      var sysPromptUser = ""
+      var sysPromptPass = ""
+
+      do {
+        sysPromptUser = readLine("Please set your Username: ")
+        sysPromptPass = readLine("Please set your Password: ")
+
+        //sysUserName
+        //sysPromptPass
+
+        doOnce = true
+      } while (doOnce != true)
+
+
+
+
+      sysUserName = sysPromptUser
+      sysPassword = sysPromptPass
+
+      println(sysUserName)
+      println(sysPassword)
+
+    }
+
+    def main(args: Array[String]): Unit = {
+      //setUp()
+      prompt()
+
+
+    }
+  }
+//}
